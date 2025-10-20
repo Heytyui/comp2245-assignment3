0079694f4f9f13175632288ee85be6dc8a1f63f1
Shanyah Payne <Payneshanyah226@gmail.com>
2025-10-19 23:18:56 -0400

    Check for winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', function () {
     const board = document.getElementById('board');
     const cells = board.getElementsByTagName('div');
+    const status = document.getElementById('status');
 
 
     for (let i = 0; i < cells.length; i++) {
@@ -29,5 +30,27 @@ window.addEventListener('DOMContentLoaded', function () {
         cells[i].addEventListener('mouseout', function () {
             cells[i].classList.remove('hover');
         });
+    }
+
+    const winning_combos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function checkWin(player) {
+        return winning_combos.some(combo => {
+            return combo.every(index => game[index] === player);
+        });
+    }
+
+    if (checkWin(currentPlayer)) {
+        status.textContent= `Player ${currentPlayer} is the Winner!`;
+        status.classList.add('you-won');
     }
 });
