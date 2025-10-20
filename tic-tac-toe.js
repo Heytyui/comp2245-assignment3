ed32b9dc2d357da4941839a9044fdd9e065053dc
Shanyah Payne <Payneshanyah226@gmail.com>
2025-10-19 22:56:09 -0400

    Change the style

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,21 +1,33 @@
-window.addEventListener('DOMContentLoaded', function(){
-    const cells = BeforeUnloadEvent.getElementsbyTagName('div');
+window.addEventListener('DOMContentLoaded', function () {
     const board = document.getElementById('board');
+    const cells = board.getElementsByTagName('div');
+
+
     for (let i = 0; i < cells.length; i++) {
         cells[i].classList.add('square');
     }
+
     const players = ['X', 'O'];
     let currentPlayer = players[0];
-
     const game = Array(9).fill(null);
+
     for (let i = 0; i < cells.length; i++) {
-        cells[i].addEventListener('click', function() {
+        cells[i].addEventListener('click', function () {
             if (!game[i]) {
                 cells[i].textContent = currentPlayer;
-                cells[i].classList.add(currentPlayer === 'X' ? 'O' : 'X');
+                cells[i].classList.add(currentPlayer === 'X' ? 'O': 'X');
 
+                
             }
         });
-    }
+
     
+        cells[i].addEventListener('mouseover', function () {
+            cells[i].classList.add('hover');
+        });
+
+        cells[i].addEventListener('mouseout', function () {
+            cells[i].classList.remove('hover');
+        });
+    }
 });
